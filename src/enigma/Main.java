5fa2f4daedbc3e1a72962cd7a0abc673e8ac3093
AnangM <social.anangmb@gmail.com>
2019-01-10 23:24:07 +0700

    Uwes Tenan

--- src/enigma/Main.java
+++ src/enigma/Main.java
@@ -11,12 +11,19 @@ package enigma;
  */
 import java.awt.datatransfer.*;
 import java.awt.Toolkit;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 public class Main extends javax.swing.JFrame {
 
     /**
      * Creates new form Main
      */
     public Main() {
+        try{
+        UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         initComponents();
        output.setEditable(false);
        inputField.requestFocus();
@@ -27,21 +34,25 @@ public class Main extends javax.swing.JFrame {
     private void encrypt(){
         int rotaryPos = rotary1.getSelectedIndex() + 1;
         int rotary2Pos = rotary2.getSelectedIndex() + 1;
+        int rotary3Pos = rotary3.getSelectedIndex() + 1;
         String input = inputField.getText().toUpperCase();
         StringBuilder chiper = new StringBuilder(input);
         
+        for(int j = 0;j<rotary3Pos;j++){
         for(int i=0;i<input.length();i++){
             char temp = input.charAt(i);
             if(rotaryPos == 26){
                 rotary2Pos += 1;
-                rotaryPos = 0;
+                if(rotary2Pos == 26){
+                    rotary2Pos = 1;
+                }
+                rotaryPos = 1;
                 rotary1.setSelectedIndex(rotaryPos);
                 rotary2.setSelectedIndex(rotary2Pos - 1);
             }
             if(!helper.isSpecialCharacter(temp)){
                     int wait = temp - 65;
                     wait = (wait + rotary2Pos) % 25;
-                    System.out.print(wait + " ");
                     wait = (wait + rotaryPos) % 25;
                     temp = (char)(wait + 65);
             }
@@ -50,20 +61,26 @@ public class Main extends javax.swing.JFrame {
             rotary1.setSelectedIndex(rotaryPos - 1);
 
         }
-        
+        }
         output.setText(chiper.toString());
     }
     
    private void decrypt(){
        int rotaryPos = rotary1.getSelectedIndex()+1;
        int rotary2Pos = rotary2.getSelectedIndex() +1;
+       int rotary3Pos = rotary3.getSelectedIndex() + 1;
+       
        String input = inputField.getText();
        StringBuilder plainText = new StringBuilder(input.toUpperCase());
+       for(int j = 0;j<rotary3Pos;j++){
        for(int i = 0; i < input.length();i++){
            char temp = input.charAt(i);
            if(rotaryPos == 26){
                 rotary2Pos += 1;
-                rotaryPos = 0;
+                if(rotary2Pos == 26){
+                rotary2Pos = 1;
+                }
+                rotaryPos = 1;
                 rotary1.setSelectedIndex(rotaryPos);
                 rotary2.setSelectedIndex(rotary2Pos - 1);
             }
@@ -81,7 +98,7 @@ public class Main extends javax.swing.JFrame {
            rotaryPos += 1;
            rotary1.setSelectedIndex(rotaryPos -1);
        }
-       System.out.println();
+       }
        output.setText(plainText.toString());
    }
     
@@ -108,17 +125,26 @@ public class Main extends javax.swing.JFrame {
         output = new javax.swing.JTextArea();
         jScrollPane2 = new javax.swing.JScrollPane();
         inputField = new javax.swing.JTextArea();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem4 = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setLocation(new java.awt.Point(0, 0));
+        setLocationByPlatform(true);
 
-        decryptButton.setText("Decrypt");
+        decryptButton.setText("Dekripsi");
         decryptButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 decryptButtonActionPerformed(evt);
             }
         });
 
-        jButton1.setText("copy");
+        jButton1.setText("Salin Hasil");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
@@ -126,7 +152,7 @@ public class Main extends javax.swing.JFrame {
         });
 
         topTitle.setFont(new java.awt.Font("Ubuntu", 1, 18)); // NOI18N
-        topTitle.setText("Simple Text Chiper");
+        topTitle.setText("Penyandi Teks Sederhana");
 
         rotary2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26" }));
         rotary2.addActionListener(new java.awt.event.ActionListener() {
@@ -139,16 +165,16 @@ public class Main extends javax.swing.JFrame {
 
         rotary3.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26" }));
 
-        lblInput.setText("Input Text");
+        lblInput.setText("Masukan");
 
-        encryptButton.setText("Encrypt");
+        encryptButton.setText("Enkripsi");
         encryptButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 encryptButtonActionPerformed(evt);
             }
         });
 
-        jLabel1.setText("Output Text");
+        jLabel1.setText("Hasil");
 
         output.setColumns(20);
         output.setRows(5);
@@ -157,6 +183,52 @@ public class Main extends javax.swing.JFrame {
         inputField.setColumns(20);
         inputField.setRows(5);
         jScrollPane2.setViewportView(inputField);
+
+        jMenu1.setText("Ubah");
+
+        jMenuItem1.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_R, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItem1.setText("Bersihkan Semua");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem1);
+
+        jMenuItem4.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_Q, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItem4.setText("Keluar");
+        jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem4ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem4);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Pilihan");
+
+        jMenuItem2.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_H, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItem2.setText("Bantuan");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem2);
+
+        jMenuItem3.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_A, java.awt.event.InputEvent.ALT_MASK | java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItem3.setText("Tentang Kami");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem3);
+
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -191,7 +263,7 @@ public class Main extends javax.swing.JFrame {
                         .addGap(85, 85, 85)
                         .addComponent(rotary1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(281, 281, 281)
+                        .addGap(260, 260, 260)
                         .addComponent(topTitle)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
@@ -223,6 +295,7 @@ public class Main extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void decryptButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_decryptButtonActionPerformed
@@ -242,6 +315,43 @@ public class Main extends javax.swing.JFrame {
     private void encryptButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_encryptButtonActionPerformed
         encrypt();
     }//GEN-LAST:event_encryptButtonActionPerformed
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        inputField.setText("");
+        output.setText("");
+        rotary1.setSelectedIndex(0);        
+        rotary2.setSelectedIndex(0);
+        rotary3.setSelectedIndex(0);
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
+        // TODO add your handling code here:
+        System.exit(0);
+    }//GEN-LAST:event_jMenuItem4ActionPerformed
+
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        // TODO add your handling code here:
+        JOptionPane.showMessageDialog(null,
+                "Bantuan :\n-Menyandikan Teks\n\tMasukkan teks pada form masukan, lalu " + 
+                 "pilihlah kunci penyandian teks anda \npada 3 pilihan diatas form masukkan "+
+                 "kemudian tekan tombol Enkripsi.\n-Membuka Teks Yang Tersandi\n\t" +
+                 "Masukkan teks yang tersandikan pada form masukan, lalu pilihlah kunci " +
+                 "yang \nsama persis ketika anda menyandikan teks tersebut, kemudian tekan" + 
+                 " tombol \nDekripsi.\n\t-Salin Hasil\n Anda dapat menyalin hasil penyandian " + 
+                 "maupun pembukaan sandi dengan \nmenekan tombol Salin Hasil",
+                "Bantuan",
+                JOptionPane.INFORMATION_MESSAGE);
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        JOptionPane.showMessageDialog(null,"Penyandi Teks Sederhana\n\n"
+                + "oleh : \n"
+                + "Nur Awalia Ramadhani (18520241024)\n"
+                + "Anang Ma'ruf Budiyanto (18520244001)\n"
+                + "Firdaus Galuh Prihasta (18520244002)\n"
+                + "Yosia Ibnu Loka (18520244014)"
+                ,"Tentang Kami",JOptionPane.INFORMATION_MESSAGE);
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -284,6 +394,13 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JTextArea inputField;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JLabel lblInput;
